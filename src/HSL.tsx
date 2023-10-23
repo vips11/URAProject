96be9095be864bb203a5922f02daa02306496342
vips11 <66628544+vips11@users.noreply.github.com>
2023-10-23 13:59:22 -0400

    implemented two algorithms

---
+++ src/HSL.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { Card } from "react-native-paper";
+
+type HSLProps = {
+  random1: number;
+  random2: number;
+  random3: number;
+};
+
+const HSL = ({ random1, random2, random3 }: HSLProps) => {
+  const hue = random1 * 360;
+  const sat = random2 * 100;
+  const lum = random2 * 100;
+  const color = `hsl(${hue}, ${sat}%, ${lum}%)`;
+  const textColor = `hsl(${360 - hue}, ${100 - sat}%, ${100 - lum}%)`;
+
+  return (
+    <View>
+      <Card
+        mode="contained"
+        style={{
+          backgroundColor: color,
+          width: 150,
+          justifyContent: "center",
+        }}
+      >
+        <Card.Title
+          title="HSL"
+          style={{ alignSelf: "center", justifyContent: "center" }}
+          titleStyle={{ color: textColor, marginTop: 10, fontSize: 20 }}
+        ></Card.Title>
+      </Card>
+    </View>
+  );
+};
+
+export default HSL;
+
+const styles = StyleSheet.create({});
